b99f4d044ee613a499e1d5d84b2d3954fc2556c4
Kai ISHIZUKA <kaiisiduka@gmail.com>
2022-08-29 14:55:35 +0900

    vscode向けgitignore変更とopenMPのサンプルコード

--- main.cpp
+++ main.cpp
@@ -19,38 +19,49 @@
 
 int main()
 {
-    int run_count = 10;
-    double ray_ene = 661.6;
-    std::vector<particle> ray_list;
-    std::vector<scinti> scintillator;
+    // int run_count = 10;
+    // double ray_ene = 661.6;
+    // std::vector<particle> ray_list;
+    // std::vector<scinti> scintillator;
 
-    ray_list.push_back(particle());
-    ray_list.back().initptcl(ray_ene, 5, 0, 0);
+    // ray_list.push_back(particle());
+    // ray_list.back().initptcl(ray_ene, 5, 0, 0);
 
-    for (int run = 0; run < run_count; run++)
-    {
-        std::vector<particle> photon;
-        photon.push_back(ray_list.at(0));
-        photon.back().initptcl(photon.back().ene_, photon.back().pt_x_, photon.back().pt_y_, photon.back().pt_z_);
-        while (0 < photon.back().ene_)
-        {
-            double total_traject_dist = 0;
-            for (int scinti_num = 0; scinti_num < scintillator.size(); scinti_num++)
-            {
-                double traject_dist = scintillator.at(scinti_num).intersec_dist(photon.back());
-                total_traject_dist += traject_dist;
-                if (traject_dist < 0.01)
-                {
-                    continue;
-                }
-                double pe_cs = pe_crosssec(photon.back().ene_, scintillator.at(scinti_num).z_),
-                    cs_ang = cs_angle(photon.back().ene_),
-                    cs_cs = kleinnishinaeq(photon.back().ene_, cs_ang);
-            }
-            if (total_traject_dist < 0.01)
-            {
-                break;
-            }
-        }
+    // for (int run = 0; run < run_count; run++)
+    // {
+    //     std::vector<particle> photon;
+    //     photon.push_back(ray_list.at(0));
+    //     photon.back().initptcl(photon.back().ene_, photon.back().pt_x_, photon.back().pt_y_, photon.back().pt_z_);
+    //     while (0 < photon.back().ene_)
+    //     {
+    //         double total_traject_dist = 0;
+    //         for (int scinti_num = 0; scinti_num < scintillator.size(); scinti_num++)
+    //         {
+    //             double traject_dist = scintillator.at(scinti_num).intersec_dist(photon.back());
+    //             total_traject_dist += traject_dist;
+    //             if (traject_dist < 0.01)
+    //             {
+    //                 continue;
+    //             }
+    //             double pe_cs = pe_crosssec(photon.back().ene_, scintillator.at(scinti_num).z_),
+    //                 cs_ang = cs_angle(photon.back().ene_),
+    //                 cs_cs = kleinnishinaeq(photon.back().ene_, cs_ang);
+    //         }
+    //         if (total_traject_dist < 0.01)
+    //         {
+    //             break;
+    //         }
+    //     }
+    // }
+    int threads_num = omp_get_max_threads();
+    std::cout << "All Threads num: " << threads_num << std::endl;
+ 
+    #pragma omp parallel for
+    for( int i = 0; i < 100000000000000000; ++i ){
+        // 並列する処理
+        
+        std::cout << "Thread No. " << omp_get_thread_num() << std::endl;
     }
+ 
+    return 0;
 }
