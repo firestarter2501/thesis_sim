7174361ee895c99a22cc0e3268f0756f45168090
Kai ISHIZUKA <kaiisiduka@gmail.com>
2022-09-15 22:01:48 +0900

    こまごまと修正

--- data/simhist.cpp
+++ data/simhist.cpp
@@ -2,7 +2,7 @@
 gROOT ->Reset();
 gStyle ->SetOptStat(1001110);
 double x,y;
-TCanvas*c1=new TCanvas("Na-22");
+TCanvas*c1=new TCanvas("sim");
 TH1S*hist=new TH1S("sim","sim",768,0,768);
 ifstream ifs("./scinti_0.dat");
 while(ifs>>x)
